9e6a0de516b016c76cc85dee4e01fd4d83d273cd
Don McDonald <dmmacs2@gmail.com>
2013-10-27 20:12:55 -0400

    Added Mind Map

---
+++ MKS_Folder_Structure.mm
@@ -0,0 +1,20 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1382918833857" ID="ID_1848272114" MODIFIED="1382918843429" TEXT="MKS_Folder_Structure">
+<node CREATED="1382918844185" ID="ID_1597243363" MODIFIED="1382918848461" POSITION="right" TEXT="Features to Add">
+<node CREATED="1382918849121" ID="ID_1770648806" MODIFIED="1382918939996" TEXT="Generate MKS Data">
+<node CREATED="1382919062605" ID="ID_342054063" MODIFIED="1382919066445" TEXT="Required">
+<node CREATED="1382918940732" ID="ID_549881175" MODIFIED="1382918948771" TEXT="Add text field for project"/>
+<node CREATED="1382918949032" ID="ID_1642025012" MODIFIED="1382918980222" TEXT="Add text field for CP (allow :none as default"/>
+<node CREATED="1382918980886" ID="ID_856815769" MODIFIED="1382919062069" TEXT="Add text field for Root path"/>
+</node>
+<node CREATED="1382919067209" ID="ID_922883196" MODIFIED="1382919068897" TEXT="Optional">
+<node CREATED="1382919077416" ID="ID_558071230" MODIFIED="1382919085696" TEXT="Allow option to select project?"/>
+<node CREATED="1382919086436" ID="ID_973629179" MODIFIED="1382919093448" TEXT="Allow option to select/create CP"/>
+<node CREATED="1382919094004" ID="ID_705546706" MODIFIED="1382919108483" TEXT="Allow option to select Root Path"/>
+</node>
+</node>
+<node CREATED="1382919147513" ID="ID_868983277" MODIFIED="1382919157448" TEXT="Add Version information to xml file"/>
+</node>
+</node>
+</map>
