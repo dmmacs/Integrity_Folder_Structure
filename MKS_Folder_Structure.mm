0c4b41459fb39c6bc6a34cce85d23066e531bdfe
Don McDonald <dmmacs@gmail.com>
2014-03-31 20:29:21 -0400

    Updated completed Bugs and Features.

--- MKS_Folder_Structure.mm
+++ MKS_Folder_Structure.mm
@@ -4,12 +4,6 @@
 <node CREATED="1382918844185" ID="ID_1597243363" MODIFIED="1382918848461" POSITION="right" TEXT="Features to Add">
 <node CREATED="1382918849121" ID="ID_1770648806" MODIFIED="1382918939996" TEXT="Generate MKS Data">
 <node CREATED="1382919062605" ID="ID_342054063" MODIFIED="1382919066445" TEXT="Required">
-<node CREATED="1382918940732" ID="ID_549881175" MODIFIED="1383238011155" TEXT="Add text field for project">
-<icon BUILTIN="button_ok"/>
-</node>
-<node CREATED="1383238014595" ID="ID_1755335434" MODIFIED="1396291901265" TEXT="Validate Root Project is valid">
-<icon BUILTIN="button_ok"/>
-</node>
 <node CREATED="1382918949032" ID="ID_1642025012" MODIFIED="1382918980222" TEXT="Add text field for CP (allow :none as default"/>
 <node CREATED="1382918980886" ID="ID_856815769" MODIFIED="1382919062069" TEXT="Add text field for Root path"/>
 </node>
@@ -23,10 +17,20 @@
 </node>
 <node CREATED="1396291515189" ID="ID_943751374" MODIFIED="1396291518946" POSITION="left" TEXT="Bugs">
 <node CREATED="1396291521048" ID="ID_1754349317" MODIFIED="1396291545086" TEXT="need a dropdown for Change Package to select :none or :bypass"/>
-<node CREATED="1396291548149" ID="ID_684941024" MODIFIED="1396291565353" TEXT="Add Window for debugging information but only display if box checked?"/>
 <node CREATED="1396291570872" ID="ID_1928425774" MODIFIED="1396291585268" TEXT="Need to understand why can&apos;t work with / as root projects"/>
 <node CREATED="1383238024922" ID="ID_776215702" MODIFIED="1383238034847" TEXT="Validate CP exists"/>
 <node CREATED="1396293067265" ID="ID_186398359" MODIFIED="1396293070614" TEXT="Fix Dirty Flag"/>
+</node>
+<node CREATED="1396312124163" FOLDED="true" ID="ID_1076454866" MODIFIED="1396312129873" POSITION="right" TEXT="Done">
+<node CREATED="1396291548149" ID="ID_684941024" MODIFIED="1396312118784" TEXT="Add Window for debugging information but only display if box checked?">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1382918940732" ID="ID_549881175" MODIFIED="1383238011155" TEXT="Add text field for project">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1383238014595" ID="ID_1755335434" MODIFIED="1396291901265" TEXT="Validate Root Project is valid">
+<icon BUILTIN="button_ok"/>
+</node>
 </node>
 </node>
 </map>
